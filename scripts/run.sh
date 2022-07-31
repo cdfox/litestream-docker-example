8dbcd77a85b915cf7ee36e6c677a8582944fcc28
Christopher Fox <chrisdavidfox@gmail.com>
2022-07-31 10:44:26 -0400

    allow start from no S3 replica

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ set -e
 
 # Restore the database.
 echo "Restoring datbase from replica"
-litestream restore -v -o /pb_data/data.db "${REPLICA_URL}"
+litestream restore -v -if-replica-exists -o /pb_data/data.db "${REPLICA_URL}"
 
 # Run litestream with your app as the subprocess.
 exec litestream replicate -exec "/usr/local/bin/pocketbase serve --http 0.0.0.0:8090 --dir /pb_data"
